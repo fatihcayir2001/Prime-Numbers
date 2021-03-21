ba8fdfe0a7c9e13f64dbf1dc31614f3d0ac96590
Fatality <fatihcayir2001@gmail.com>
2021-03-21 15:16:36 +0300

    Add project files.

---
+++ AsalSayılar/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Transactions;
+
+namespace AsalSayılar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool notPrime = false;
+            Console.WriteLine("Enter your target");
+            int target = Int32.Parse(Console.ReadLine());
+            int j;
+            for (int i = 2; i < target; i++)
+            {
+                for (j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        notPrime = true;
+                        break;
+                    }
+                    
+                }
+
+                if (notPrime == false)
+                {
+                    Console.WriteLine(j);
+                }
+                else
+                    notPrime = false;
+            }
+
+        }
+
+        
+    }
+}
